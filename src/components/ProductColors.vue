6bcf380ec8cde3a9e411f4b7b35ab8931b9a3303
IlyaMyachin <97542058+IlyaMyachin@users.noreply.github.com>
2022-06-20 13:26:14 +0300

    module 4 homework edits 1

--- src/components/ProductColors.vue
+++ src/components/ProductColors.vue
@@ -2,7 +2,7 @@
   <ul class="colors">
     <li class="colors__item" v-for="color in colors" :key="color">
       <label class="colors__label">
-        <input class="colors__radio sr-only" type="radio" name="color" :value="color" v-model="currentColor">
+        <input class="colors__radio sr-only" type="radio" name="color" :value="color" v-model="colorPick">
         <span class="colors__value" :style="{ backgroundColor: color }">
         </span>
       </label>
@@ -14,14 +14,14 @@
 
 export default {
   props: ['colors', 'selectedColor'],
-  data() {
-    return {
-      currentColor: ''
-      }
-  },
   computed: {
-    colorPick() {
-      return this.$emit('update:selectedColor', this.currentColor);
+    colorPick: {
+      get() {
+        return this.selectedColor;
+      },
+      set(value) {
+        return this.$emit('update:selectedColor', value)
+      }
     }
   }
 }
